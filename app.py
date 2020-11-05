f12c00ba9c48bd29276ac6318bb60f68face1c88
Akriti Suri <akritisuri96@gmail.com>
2020-11-04 23:02:49 -0800

    one

--- app.py
+++ app.py
@@ -9,4 +9,15 @@ from flask import Flask, jsonify
 
 engine = create_engine("sqlite:///hawaii.sqlite")
 
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys
+
+#Save Table References
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+sessions = Session(engine)
+
+app = Flask(_name_)
 
